e582d6918b82b589a2f2312028148b51e91d5ce6
menoueranes <menoueranes@gmail.com>
2026-08-25 02:20:08 +0100

    initia

--- COMPILEDZ.py
+++ COMPILEDZ.py
@@ -1,17 +1,16 @@
 a=[0] * 101
 for i in range(1, 101):
      a[i] = f"{i} is off"
-for i in range(1, 101):
-     if int(a[i].replace(" is off", "")) % i == 0 or int(a[i].replace(" is on", "")) % i == 0:
-         if a[i].endswith("off"):
+
+for j in range(1, 101):     
+    for i in range(1, 101):
+        if i % j == 0:
+           if a[i].endswith("off"):
                 a[i] = f"{i} is on"
-         elif a[i].endswith("on"):
+           else:
                 a[i] = f"{i} is off"
-     print(a[i])
-
-    
-
-
+    if a[j].endswith("on"):
+        print(a[j])
 
 
 
