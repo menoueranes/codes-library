2ea4b5ff05773ae2a343577ea8e96c82ee0ab9c1
menoueranes <menoueranes@gmail.com>
2026-09-03 00:43:10 +0100

    initial commit

---
+++ Python/reel.py
@@ -0,0 +1,16 @@
+x = 15
+y = 20
+if x > 10:
+     if y > 15:
+          print("A")
+     else:
+          print("B")
+elif x > 5:
+     print("C")
+else:
+     print("D")                    
+
+
+
+
+     
